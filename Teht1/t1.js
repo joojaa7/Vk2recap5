90c7fe2dfc8b8c3b0b35a0ddd5ebb6937b43dc59
Joojaa7 <mikael.asikainen@metropolia.fi>
2024-03-29 19:15:20 +0200

    m

--- Teht1/t1.js
+++ Teht1/t1.js
@@ -1,6 +1,6 @@
 
 async function query(){
-  const response = await fetch('https://reqres.in/api/users/1');
+  const response = await fetch('https://10.120.32.94/api/v1/restaurants');
   const json = await response.json();
   console.log(json);
 }
---
+++ Teht5/t5.js
@@ -0,0 +1,85 @@
+
+const makeFetch = async (url) => {
+  const result = await fetch(url)
+
+  return await result.json()
+}
+
+const fetchRestaurants = async () =>
+  await makeFetch("https://10.120.32.94/restaurant/api/v1/restaurants")
+
+const fetchDailyMenu = async (id) =>
+  makeFetch(`https://10.120.32.94/restaurant/api/v1/restaurants/daily/${id}/fi`)
+
+
+const sortRestaurants = (restaurants) => {
+  restaurants.sort((a, b) =>
+    a.name
+      .toLowerCase()
+      .trim()
+      .localeCompare(b.name.toLowerCase().trim())
+  )
+}
+
+const createPhoneLink = (phone) => {
+  const cleanedNumber = phone.replaceAll(" ", "").replace(/[a-zA-Z-]+/g, "")
+
+  return `<a href="tel:${cleanedNumber}">${cleanedNumber}</a>`
+}
+
+const createDialog = (restaurant, dialogNode, menu) => {
+  const phone = restaurant.phone !== "-" ? createPhoneLink(restaurant.phone) : ""
+
+  dialogNode.innerHTML = `
+    <h1>${restaurant.name}</h1>
+    <p>${restaurant.address}, ${restaurant.postalCode} ${restaurant.city}</p>
+    <p>${restaurant.company} ${phone}</p>
+
+    <ul>
+    ${menu.courses.map(({name, price, diets}) => `<li>${name} - ${price} (${diets.join(", ")})</li>`).join("")}
+    </ul>
+
+    <form method="dialog">
+      <button>Sulje</button>
+    </form>
+  `
+  dialogNode.showModal()
+}
+
+const handleTableRowClick = async (tr, restaurant, dialogNode) => {
+  document.querySelectorAll("tr").forEach((tr) => {
+    tr.classList.remove("highlight")
+  })
+
+  tr.classList.add("highlight")
+
+  const menu = await fetchDailyMenu(restaurant._id)
+  console.log("menu", menu)
+
+  createDialog(restaurant, dialogNode, menu)
+}
+
+const createTable = (restaurants) => {
+  const tableNode = document.querySelector("table")
+  const dialogNode = document.querySelector("dialog")
+
+  restaurants.forEach((restaurant) => {
+    const tr = document.createElement("tr")
+    tr.innerHTML = `<td>${restaurant.name}</td><td>${restaurant.address}</td>`
+    tableNode.appendChild(tr)
+
+    tr.addEventListener("click", () => {
+      handleTableRowClick(tr, restaurant, dialogNode)
+    })
+  })
+}
+
+const buildWebsite = async () => {
+
+  const restaurants = await fetchRestaurants()
+  sortRestaurants(restaurants)
+
+  createTable(restaurants)
+}
+
+buildWebsite()
